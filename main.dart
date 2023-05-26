2b14e873a59ad699a178a6e0bd78afc6019edd95
pouya <hadishhamraz@gmail.com>
2023-05-26 23:02:03 +0330

    test

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget 
+{
+  @override
+  Widget build(BuildContext context) => MaterialApp
+  (
+    home: Text('Hello, World!'),
+  );
+}
