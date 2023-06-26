2a4f502bee10fc4f55b5c2136997ea8f2508c10b
pouya <hadishhamraz@gmail.com>
2023-06-26 23:42:23 +0300

    purchase & travels page

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'search.dart';
+import 'travels.dart';
+import 'purchase.dart';
 
 class app extends StatelessWidget {
   @override
@@ -15,10 +17,107 @@ class app extends StatelessWidget {
             ),
           ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 80.0,top:20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      width: 120.0,
+                      height: 40.0,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                              transitionDuration: Duration(milliseconds: 400),
+                              pageBuilder: (_, __, ___) => search(),
+                              transitionsBuilder:
+                                  (_, animation, __, child) {
+                                return SlideTransition(
+                                  position: Tween<Offset>(
+                                    begin: Offset(0.0, 2.0),
+                                    end: Offset.zero,
+                                  ).animate(animation),
+                                  child: child,
+                                );
+                              },
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          textStyle: TextStyle(fontSize: 16.0),
+                        ),
+                        icon: Icon(Icons.person),
+                        label: Text('پروفایل'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 120.0,
+                      height: 40.0,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                              transitionDuration: Duration(milliseconds: 400),
+                              pageBuilder: (_, __, ___) => travels(),
+                              transitionsBuilder:
+                                  (_, animation, __, child) {
+                                return SlideTransition(
+                                  position: Tween<Offset>(
+                                    begin: Offset(0.0, 2.0),
+                                    end: Offset.zero,
+                                  ).animate(animation),
+                                  child: child,
+                                );
+                              },
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          textStyle: TextStyle(fontSize: 16.0),
+                        ),
+                        icon: Icon(Icons.card_travel),
+                        label: Text('سفر ها'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 120.0,
+                      height: 40.0,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                              transitionDuration: Duration(milliseconds: 400),
+                              pageBuilder: (_, __, ___) => purchase(),
+                              transitionsBuilder:
+                                  (_, animation, __, child) {
+                                return SlideTransition(
+                                  position: Tween<Offset>(
+                                    begin: Offset(0.0, 2.0),
+                                    end: Offset.zero,
+                                  ).animate(animation),
+                                  child: child,
+                                );
+                              },
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          textStyle: TextStyle(fontSize: 14.0),
+                        ),
+                        icon: Icon(Icons.credit_card),
+                        label: Text('تراکنش ها'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Text(
-                'نوع بلیط را انتخاب کنید',
+                'خرید بلیط',
                 style: TextStyle(
                   fontSize: 30,
                 ),
@@ -29,22 +128,23 @@ class app extends StatelessWidget {
                 height: 60.0,
                 child: ElevatedButton.icon(
                   onPressed: () {
-                Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                    transitionDuration: Duration(milliseconds: 400),
-                    pageBuilder: (_, __, ___) => search(),
-                    transitionsBuilder: (_, animation, __, child) {
-                      return SlideTransition(
-                        position: Tween<Offset>(
-                          begin: Offset(0.0, 2.0),
-                          end: Offset.zero,
-                        ).animate(animation),
-                        child: child,
-                      );
-                    },
-                  ),
-                );
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        transitionDuration: Duration(milliseconds: 400),
+                        pageBuilder: (_, __, ___) => search(),
+                        transitionsBuilder:
+                            (_, animation, __, child) {
+                          return SlideTransition(
+                            position: Tween<Offset>(
+                              begin: Offset(0.0, 2.0),
+                              end: Offset.zero,
+                            ).animate(animation),
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
                   },
                   style: ElevatedButton.styleFrom(
                     textStyle: TextStyle(fontSize: 30.0),
