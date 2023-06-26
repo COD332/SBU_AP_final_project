5580ed4fdd7582015c34bd823431131f955abf86
pouya <hadishhamraz@gmail.com>
2023-06-26 23:43:32 +0300

    purchase & travels page 2

---
+++ lib/purchase.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+class purchase extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+                Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                         SizedBox(
+                        width: 130.0,
+                        height: 50.0,
+                        child: ElevatedButton.icon(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            textStyle: TextStyle(fontSize: 15.0),
+                            backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            ),
+                        ),
+                        icon: Container(),
+                        label: Text('افزایش اعتبار'),
+                        ),
+                    ),
+                    SizedBox(width: 20),
+                  SizedBox(
+                    width: 170,
+                    child: TextField(
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 13),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.attach_money),
+                        labelText: 'مبلغ',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  
+                ],
+              ),
+              SizedBox(height: 20),
+              SizedBox(height: 30.0),
+              Table(
+                border: TableBorder.all(),
+                children: [
+                  TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              color: Colors.grey[300],
+                              child: Center(child: Text('مبلغ')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              color: Colors.grey[300],
+                              child: Center(child: Text('تاریخ')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              color: Colors.grey[300],
+                              child: Center(child: Text('کد رهگیری')),
+                            ),
+                          ),
+                        ],
+                      ),
+                                        TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              color: Colors.green[200],
+                              child: Center(child: Text('+۱۰۰,۰۰۰')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۴۰۱ / ۳ / ۲۵')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۵۴۷۲۳۸۲۹۳۸')),
+                            ),
+                          ),
+                        ],
+                      ),
+                                        TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              color: Colors.red[200],
+                              child: Center(child: Text('-۱۰۰,۰۰۰')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۴۰۱ / ۳ / ۲۵')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۵۴۷۲۳۸۲۹۳۸')),
+                            ),
+                          ),
+                        ],
+                      ),
+                                                              TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              color: Colors.green[200],
+                              child: Center(child: Text('+۱۰۰,۰۰۰')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۴۰۱ / ۳ / ۲۵')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۵۴۷۲۳۸۲۹۳۸')),
+                            ),
+                          ),
+                        ],
+                      ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/travels.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+
+class travels extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
+                child: SizedBox(
+                  width: 300,
+                  child: TextField(
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.send),
+                      labelText: 'کد سفر',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+                            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 140,
+                    child: TextField(
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 13),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.date_range),
+                        labelText: 'تا تاریخ',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  SizedBox(
+                    width: 140,
+                    child: TextField(
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 13),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.date_range),
+                        labelText: 'از تاریخ',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(fontSize: 25.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+                icon: Icon(Icons.search),
+                label: Text('جست و جو'),
+              ),
+              SizedBox(height: 30.0),
+              Table(
+                border: TableBorder.all(),
+                children: [
+                  TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              color: Colors.grey[300],
+                              child: Center(child: Text('هزینه')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              color: Colors.grey[300],
+                              child: Center(child: Text('تاریخ حرکت')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              color: Colors.grey[300],
+                              child: Center(child: Text('کد سفر')),
+                            ),
+                          ),
+                        ],
+                      ),
+                                        TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۰۰,۰۰۰')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۴۰۱ / ۳ / ۲۵')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۱۱۱۱')),
+                            ),
+                          ),
+                        ],
+                      ),
+                                        TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۰۰,۰۰۰')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۴۰۱ / ۳ / ۲۵')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۱۱۱۱')),
+                            ),
+                          ),
+                        ],
+                      ),
+                                                              TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۰۰,۰۰۰')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۴۰۱ / ۳ / ۲۵')),
+                            ),
+                          ),
+                          TableCell(
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('۱۱۱۱۱')),
+                            ),
+                          ),
+                        ],
+                      ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
