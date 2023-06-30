23a387abed10c675d5873556680b2b1502a60f63
Pouya Hamraz <110659145+hadish100@users.noreply.github.com>
2023-06-30 10:39:35 +0330

    discount backend done

---
+++ src/Discount.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*; 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+
+public class Discount
+{
+    public static float use_discount(int orig_price,String code) throws IOException
+    {
+        File obj = new File("Discount.txt");
+        Scanner myReader = new Scanner(obj);
+        String[] data;
+
+        while (myReader.hasNextLine()) 
+        {
+          data = myReader.nextLine().split("-");
+          if(data[0].equals(code)) return orig_price*((float)(100-Integer.parseInt(data[1]))/100);
+        }
+
+        return orig_price;
+    }
+
+    //TEST
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println(use_discount(1000,"yalda1400"));
+        System.out.println(use_discount(1000,"yalda1399"));
+    }
+}
